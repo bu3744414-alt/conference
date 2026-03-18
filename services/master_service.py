cffcc23debb27539d4e59c3307c111bca356d6d5
unknown <Administrator@INHYD-L-5004.vslp.in>
2026-03-18 14:32:29 +0530

    fix master_service pymssql error

--- services/master_service.py
+++ services/master_service.py
@@ -4,28 +4,39 @@ from database.db import get_connection
 def get_halls():
 
     conn = get_connection()
+    cursor = conn.cursor()
 
-    rows = conn.execute("""
+    cursor.execute("""
         SELECT conference_id, conference_name
         FROM conference_master
         WHERE status='A'
         ORDER BY conference_id
-    """).fetchall()
+    """)
 
+    rows = cursor.fetchall()
+
+    cursor.close()
     conn.close()
 
     return rows
+
+
 # --- GET ACTIVE DEPARTMENTS ---
 def get_departments():
-    conn = get_connection()
 
-    rows = conn.execute("""
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
         SELECT DISTINCT department
         FROM Login_mas
         WHERE status_flg='A'
         ORDER BY department
-    """).fetchall()
+    """)
 
+    rows = cursor.fetchall()
+
+    cursor.close()
     conn.close()
 
     return [r[0] for r in rows]
